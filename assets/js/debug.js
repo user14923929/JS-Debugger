eb3f64a48d585d8f54c8f65bb26612b2cffcad11
IlliaCoder <153951865+user14923929@users.noreply.github.com>
2025-02-21 17:39:45 +0200

    Fixing debug.js

--- assets/js/debug.js
+++ assets/js/debug.js
@@ -3,7 +3,7 @@ function debug() {
     const steps = parseInt(document.getElementById('steps').value, 10);
     const output = document.getElementById('output');
 
-    const PROHIBITION_SYMBOLS = ["<", ">", "/", "script", "alert", "prompt", "confirm", "console", "log", "debugger", "eval", "function", "window", "document", "location", "history", "localStorage", "sessionStorage", "cookie", "fetch", "XMLHttpRequest", "setTimeout", "setInterval", "clearTimeout", "clearInterval", "addEventListener", "removeEventListener", "onload", "onunload", "onbeforeunload", "onerror", "onabort", "onresize", "onscroll", "onfocus", "onblur", "onchange", "onsubmit", "onreset", "onselect", "oninput", "onkeydown", "onkeypress", "onkeyup", "onclick", "ondblclick", "onmousedown", "onmouseup", "onmouseover", "onmousemove", "onmouseout", "onmouseenter", "onmouseleave", "oncontextmenu", "onwheel", "ontouchstart", "ontouchend", "ontouchmove", "ontouchcancel", "onpointerover", "onpointerenter", "onpointerdown", "onpointermove", "onpointerup", "onpointercancel", "onpointerout", "onpointerleave", "onpointerlockchange", "onpointerlockerror", "onfullscreenchange", "onfullscreenerror", "onreadystatechange", "onloadstart", "onprogress", "onabort", "onerror", "onload", "onloadend", "oncanplay", "oncanplaythrough", "onplay", "onplaying", "onpause", "onended", "onvolumechange", "onratechange", "onseeked", "onseeking", "onstalled", "onsuspend", "ontimeupdate", "onwaiting", "onmessage", "onopen", "onerror", "onclose"];
+    const PROHIBITION_SYMBOLS = ["<", ">", "/"];
 
     if (text.value.length === 0) {
         output.innerHTML = 'Please enter a text.';
